f01f5f6215c3de4712a1a704fac76e3b78c132cd
Nicholas Rallakis <nrallakis4@gmail.com>
2021-12-16 19:12:15 +0200

    Add gitignore + test simple db queries

---
+++ api/database.js
@@ -0,0 +1,21 @@
+var mysql = require('mysql');
+
+function executeQuery(req, res, query) {
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "multe-pass"
+    });
+
+    con.connect((err) => {
+        if (err) throw err;
+        con.query(query, (err, result, fields) => {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
+    });
+};
+
+module.exports = { executeQuery };
--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
+const { executeQuery } = require('./database');
 const express = require('express');
-const app = express();
 const port = 8000;
+const app = express();
 
 app.listen(port, () => {
     console.log('App is running on port ${port}!');
 });
 
 app.get('/', (req, res) => {
-    res.send("hello world");
+    executeQuery(req, res, "SELECT * FROM pass LIMIT 100");
+    // res.send("Hello world");
+    // res.send(passes);
 });
