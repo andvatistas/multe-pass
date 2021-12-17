7a490578a7c71292a097377946935882ed716e86
Andreas Va <andvatistas@gmail.com>
2021-12-17 19:09:04 +0200

    link files and basis for api queries

---
+++ api/endpoints/passesAnalysis.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+var mysql = require('mysql');
+
+function passesAnalysis(req,res){
+
+  var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "multe-pass"
+  });
+	con.connect(function(err) {
+		if (err) throw err;
+		console.log("Connected!");
+		let limiter=req.query.limit;
+		let myQuery="SELECT * FROM pass WHERE pass.id="+"'"+req.params.op_ID+"'"; //
+		if(limiter==undefined || Number.isInteger(Number(limiter))==false){}
+			else{
+				myQuery=myQuery +" LIMIT " +Number(limiter);
+			}
+
+			console.log(myQuery)
+			con.query(myQuery, function (err, result, fields){
+				if (err) throw err;
+				res.send(result);
+			});
+		});
+
+
+}
+
+router.get('/PassesAnalysis/:op1_ID/:op2_ID/:date_from/:date_to',passesAnalysis)
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const { executeQuery } = require('./database');
+// const { executeQuery } = require('./database');
 const express = require('express');
 const port = 8000;
 const app = express();
@@ -7,8 +7,22 @@ app.listen(port, () => {
     console.log('App is running on port ${port}!');
 });
 
+
+
 app.get('/', (req, res) => {
-    executeQuery(req, res, "SELECT * FROM pass LIMIT 100");
-    // res.send("Hello world");
-    // res.send(passes);
+    res.send("Hello world -- use API parameters in URL to access DATA");
 });
+
+
+// load all endpoints
+const chargesBy=require("./endpoints/chargesBy.js");
+const passesAnalysis=require("./endpoints/passesAnalysis.js");
+const passesCost=require("./endpoints/passesCost.js");
+const passesPerStation=require("./endpoints/passesPerStation.js");
+
+//bind all endpoints to app router
+//router -> when the URL is modified somehow, it will detect that change and render the view that is associated with the new URL
+app.use('/',chargesBy);
+app.use('/',passesAnalysis);
+app.use('/',passesCost);
+app.use('/',passesPerStation);
