5ffc24b4a613209356e7fab58d73ba239d12c1f0
Nicholas Rallakis <nrallakis4@gmail.com>
2021-12-19 00:01:19 +0200

    Move db connection to another file + refactoring
    Now we keep only one connection to the db.

--- api/index.js
+++ api/index.js
@@ -8,12 +8,9 @@ app.listen(port, () => {
     console.log(`App is running on port ${port}!`);
 });
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
-
 
 // load all endpoints
 const chargesBy = require("./endpoints/chargesBy.js");
@@ -21,7 +18,7 @@ const passesAnalysis = require("./endpoints/passesAnalysis.js");
 const passesCost = require("./endpoints/passesCost.js");
 const passesPerStation = require("./endpoints/passesPerStation.js");
 
-//bind all endpoints to app router
+//bind all endpoints to app router with base url
 //router -> when the URL is modified somehow, it will detect that change and render the view that is associated with the new URL
 const baseUrl = '/interoperability/api';
 app.use(baseUrl, chargesBy);
