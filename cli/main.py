c5e7718f7da4fa71e9f701a7f6a6f91664f520f2
bvrettos <billvrettos2000@gmail.com>
2021-12-31 19:10:48 +0200

    1st working version of CLI, everyone has covid, Kalh Xronia :)

--- cli/main.py
+++ cli/main.py
@@ -7,24 +7,54 @@ import sys
 import pandas as pd
 
 
-#Positional Arguments (Scope)
+
+#Parser Initialization
 parser = argparse.ArgumentParser(description = 'CLI Interoperability API')
-parser.add_argument('healthcheck', help = 'Check Database Connection Status')
-parser.add_argument('resetpasses', help = 'Reset Passes Table from DB')
-parser.add_argument('resetstations', help = 'Reset Stations Table from DB')
-parser.add_argument('resetvehicles', help = 'Reset Vehicles Table from DB')
 
-#Option Arguments / Parameter Arguments
-parser.add_argument('--format', help = 'select format, either JSON (default) or CSV')
+#Format Parser
+formatparse = argparse.ArgumentParser(add_help=False)
+formatparse.add_argument('--format', help = 'Select output format', default = "json", type = str, choices = ['json','csv'])
+#SubParsers
+sp = parser.add_subparsers(help='Subparser Init')
 
-args = parser.parse_args()
+#Health Check
+sp_healthcheck = sp.add_parser('healthcheck', parents = [formatparse], help = 'Check Database Connection Status')
 
+#Reset Functions
+sp_resetvehicles = sp.add_parser('resetvehicles', parents = [formatparse], help = 'Reset Vehicles Table from DB')
+sp_resetpasses = sp.add_parser('resetpasses', parents = [formatparse], help = 'Reset Passes Table from DB')
+sp_resetstations = sp.add_parser('resetstations', parents = [formatparse], help = 'Reset Stations Table from DB')
 
-r = requests.get("http://localhost:9103/interoperability/api/admin/healthcheck")
-r2 = requests.get("http://localhost:9103/interoperability/api/PassesPerStation/AO01/20201201/20210101")
-# pprint(r.json())
+#Passes Per Station
+sp_passesperstation = sp.add_parser('passesperstation', parents = [formatparse], help = 'See all passes made through a station for a specific time period')
+sp_passesperstation.add_argument('--station')
+sp_passesperstation.add_argument('--datefrom')
+sp_passesperstation.add_argument('--dateto')
 
+#Passes Analysis
+sp_passesanalysis = sp.add_parser('passesanalysis', parents = [formatparse], help = 'Passes Analysis between 2 Operators')
+sp_passesanalysis.add_argument('--op1')
+sp_passesanalysis.add_argument('--op2')
+sp_passesanalysis.add_argument('--datefrom')
+sp_passesanalysis.add_argument('--dateto')
 
+#Passes Cost
+sp_passescost = sp.add_parser('passescost', parents = [formatparse], help = 'Cost of passes between 2 Operators')
+sp_passescost.add_argument('--op1')
+sp_passescost.add_argument('--op2')
+sp_passescost.add_argument('--datefrom')
+sp_passescost.add_argument('--dateto')
+
+#Charges By
+sp_chargesby = sp.add_parser('chargesby', parents = [formatparse], help = 'Passes from vehicles belonging to other Operators')
+sp_chargesby.add_argument('--op')
+sp_chargesby.add_argument('--datefrom')
+sp_chargesby.add_argument('--dateto')
+
+#Create the Namespace
+ns = parser.parse_args()
+
+#Output (format) functions
 def print_json(request):
     json_data = request.json()
     pprint(json_data)
@@ -33,13 +63,37 @@ def print_csv(request):
     json_data = request.json()
     df = pd.json_normalize(json_data)
     df.to_csv(sys.stdout)
-# def print_csv(request):
-#     json_data = request.json()
-#     pprint(json_data)
-#     csv_data = csv.writer(sys.stdout)
-#     csv_data.writerow(json_data[0].keys())
-#     print("hi")
-#     for row in json_data:
-#         csv_data.writerow(row.values())
-#print_json(r2)
-#print_csv(r2)
+
+#Switch - Case ( peripoy :) )
+if sys.argv[1] == 'healthcheck':
+    print('healtcheck')
+    request_string = "http://localhost:9103/interoperability/api/admin/healthcheck"
+    request = requests.get(request_string)
+elif sys.argv[1] == 'resetvehicles':
+    request_string = "http://localhost:9103/interoperability/api/admin/resetvehicles"
+    request = requests.post(request_string)
+elif sys.argv[1] == 'resetpasses':
+    request_string = "http://localhost:9103/interoperability/api/admin/resetpasses"
+    request = requests.post(request_string)
+elif sys.argv[1] == 'resetstations':
+    request_string = "http://localhost:9103/interoperability/api/admin/resetstations"
+    request = requests.post(request_string)
+elif sys.argv[1] == 'passesperstation':
+    request_string = "http://localhost:9103/interoperability/api/passesperstation/" + str(ns.station) + "/" + str(ns.datefrom) + "/" + str(ns.dateto)
+    request = requests.get(request_string)
+elif sys.argv[1] == 'passesanalysis':
+    request_string = "http://localhost:9103/interoperability/api/PassesAnalysis/" + str(ns.op1) + "/" + str(ns.op2) + "/" + str(ns.datefrom) +"/" + str(ns.dateto)
+    request = requests.get(request_string)
+elif sys.argv[1] == 'passescost':
+    request_string = "http://localhost:9103/interoperability/api/PassesCost/" + str(ns.op1) + "/" + str(ns.op2) + "/" + str(ns.datefrom) +"/" + str(ns.dateto)
+    request = requests.get(request_string)
+elif sys.argv[1] == 'chargesby':
+    request_string = "http://localhost:9103/interoperability/api/ChargesBy/" + str(ns.op) + "/" + str(ns.datefrom) + "/" + str(ns.dateto)
+    request = requests.get(request_string)
+
+#Format Checks
+if ns.format == 'json':
+    print_json(request)
+
+if ns.format == 'csv':
+    print_csv(request)
