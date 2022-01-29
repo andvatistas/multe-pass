c4245f4bc02b008489f4a6149abfb762b732c18a
avgoustin0 <avgoustinos.0@gmail.com>
2022-01-29 22:26:05 +0200

    Added passesPerStation

---
+++ frontend/pages/passesPerStation.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <meta charset = "utf-8">
+    <meta name = "viewport" content = "width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet"  href="../custom_design.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Multe-Pass Charges By</title>
+  </head>
+
+  <body>
+    <?php include '../components/header.php';?>
+
+<h4 class = "d-flex justify-content-center m-3 p-1">Charges By Form</h4>
+<div class = "container d-flex justify-content-center" >
+  <form  action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = 'POST'>
+    <div class = "form-inline" style = "width:100%;">
+      <label class="label">Station</label>
+      <input type="text" class = "form-control" name = "stationid">
+    </div>
+    <div class = "row row-cols-2" >
+      <div class="form-group date" name = "datefrom" style = "padding-top:5px;">
+        <label class="label">Period Start</label>
+        <input type="date" class = "form-control" name = "datefrom" id="datepicker">
+      </div>
+      <div class="form-group date"name = "dateto" style = "padding-top:5px;">
+        <label class="label">Period End</label>
+        <input type="date" class = "form-control" name = "dateto" id="datepicker">
+      </div>
+  </div>
+  <div class = "d-flex justify-content-center" style = "padding-top:20px;">
+    <button id = "main_button" type="submit" class="btn btn-primary">Send Request</button>
+  </div>
+</form>
+</div>
+
+<?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include '../components/helpers.php';
+    $stationid = $_POST["stationid"];
+    $datefromraw =  date_create($_POST["datefrom"]);
+    $datetoraw =  date_create($_POST["dateto"]);
+    $datefrom = date_format($datefromraw,"Ymd");
+    $dateto = date_format($datetoraw,"Ymd");
+    $api_url = 'http://localhost:9103/interoperability/api/passesperstation/' . $stationid . '/' . $datefrom . '/' . $dateto . '/';
+    $request_method = 'GET';
+    $response = sendRequest($api_url, $request_method);
+    $json_response = json_decode($response);
+  }?>
+
+  <br>
+  <?php   if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (($stationid == '') || ($datefrom == '') || ($dateto == '')){
+      echo "<div class = 'container d-flex justify-content-center'>
+              <div class = 'card'>
+                <div class = 'card-body'>
+                  <p> Please input data into every form!</p>";
+                  echo" </div>";
+                  echo" </div>";
+                  echo" </div>";
+    }
+    else {
+    echo "<div class = 'container d-flex justify-content-center'>
+      <div class = 'card' >
+        <div class = 'card-body'>";
+        if ($json_response == null){
+          echo "<p>No Data Found! Check your Request Form!</p>";
+          echo" </div>";
+          echo" </div>";
+          echo" </div>";
+          }
+          else {
+          echo "
+          <table class = 'passescosttable'>
+            <thead>
+              <tr>
+                <th>Station ID</th>
+                <th>Station Operator</th>
+                <th>Request Timestamp</th>
+                <th>Period From</th>
+                <th>Period To</th>
+                <th>Number of Passes</th>
+              </tr>
+            </thead>";
+
+          echo "<tr>";
+          echo "<td>".$json_response->Station."</td>";
+          echo "<td>".$json_response->StationOperator."</td>";
+          echo "<td>".$json_response->RequestTimestamp."</td>";
+          echo "<td>".$json_response->PeriodFrom."</td>";
+          echo "<td>".$json_response->PeriodTo."</td>";
+          echo "<td>".$json_response->NumberOfPasses."</td>";
+          echo "</tr>";
+          echo "</table>";
+
+
+
+          echo "<br>";
+          echo "<div class='b-example-divider'></div>";
+          echo "<table class = 'passescosttable'>
+            <thead>
+              <tr>
+                <th>Pass Index</th>
+                <th>Pass ID</th>
+                <th>Passes Timestamp</th>
+                <th>Vehicle ID</th>
+                <th>Tag Provider</th>
+                <th>Pass Charge</th>
+                <th>Pass Type</th>
+              </tr>
+            </thead>";
+              foreach($json_response->PassesList as $elem){
+              echo "<tr>";
+              echo "<td>".$elem->PassIndex ."</td>";
+              echo "<td>".$elem->PassID ."</td>";
+              echo "<td>".$elem->PassTimeStamp ."</td>";
+              echo "<td>".$elem->VehicleID ."</td>";
+              echo "<td>".$elem->TagProvider ."</td>";
+              echo "<td>".$elem->PassCharge ."</td>";
+              echo "<td>".$elem->PassType ."</td>";
+              echo "</tr>";
+            }
+            echo "</table>";
+            echo" </div>";
+            echo" </div>";
+            echo" </div>";
+        }
+      }
+    }?>
+        <br>
+  <!-- Footer -->
+  <?php include '../components/footer.php';?>
+
+    </body>
+
+
+  </html>
